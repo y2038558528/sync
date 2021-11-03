89f502b644dab652d9d9a4868e07f5651762159a
y2038558528 <62008666+y2038558528@users.noreply.github.com>
2021-11-03 17:09:38 +0800

    Delete frp.sh

--- frp.sh
+++
@@ -1,6 +0,0 @@
-wget -P /usr/local https://ghproxy.com/https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_arm64.tar.gz
-cd /usr/local
-tar -zxvf frp_0.38.0_linux_arm64.tar.gz  -C /usr/local/ 
-mv frp_0.38.0_linux_arm64 frp
-cd systemd
-wget 
