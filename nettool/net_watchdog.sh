245deb2a6c15179e4c0936b2c3561514a06c481e
y2038558528 <62008666+y2038558528@users.noreply.github.com>
2021-12-02 11:13:39 +0800

    Add files via upload

---
+++ nettool/net_watchdog.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# 自动检查线路是否正常，连续30秒不正常就重启 Openwrt 重新拨号
+# 本脚本以湖北移动dns服务器ip为检测标准,如需其他运营商或者地区,请选择修改ip地址
+
+PING='ping -c 5 211.137.64.163|grep -v grep|grep "64 bytes" |wc -l'
+
+if [ ${PING} -ne 0 ];then
+    exit 0
+else
+    /etc/init.d/network restart
+fi
+
+sleep 30
+
+PING2='ping -c 5 211.137.64.163|grep -v grep|grep "64 bytes" |wc -l'
+
+if [ ${PING2} -ne 0 ];then
+    exit 0
+else
+    reboot -nf
+fi
+
+### 将文件放到/usr/ 目录�
+### crontab -e
+### 添加内容 1-59/5 * * * * /usr/net_watchdog.sh
+### reboot -nf 为强制不保存信息重启.如不正常可修改为 reboot
