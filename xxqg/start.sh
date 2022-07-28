0e5306a7aa888a9cc5344815df41569e3c74678e
y2038558528 <62008666+y2038558528@users.noreply.github.com>
2022-07-28 12:55:11 +0800

    Update and rename 1 to start.sh

---
+++ xxqg/start.sh
@@ -0,0 +1,8 @@
+## 下载pip3安装库
+wget https://raw.githubusercontent.com/y2038558528/sync/main/xxqg/get-pip.py
+## 安装pip3库
+python3 get-pip.py
+## 使用pip3安装playwright
+pip3 install playwright
+## 安装浏览器驱动（安装过程稍微有点慢，请耐心等待）
+python3 -m playwright install
